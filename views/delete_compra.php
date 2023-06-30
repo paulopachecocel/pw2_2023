5d8845c5d0bf381414f672b17e3d9378f317484c
Paulo Pacheco <paulopachecocel@gmail.com>
2023-06-30 11:16:17 -0300

    20230630-11h15-Compra

---
+++ views/delete_compra.php
@@ -0,0 +1,11 @@
+<?php
+include_once("restrict.php");
+require_once "controllers/CompraController.php";
+
+if (isset($_GET["id"])) {
+    $compraController = new CompraController();
+    $compraController->delete($_GET["id"]);
+
+    // Voltando pra tela anterior
+    header("Location: ?pg=compras");
+}
